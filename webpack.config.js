d1dc85ae4dd2f8b6ed4b225f66e98bf0a8a30a22
Will <will_constable@msn.com>
2021-08-01 16:49:02 +0100

    tweaks

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
   module: {
     rules: [
